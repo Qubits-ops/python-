de05aedce339688a37a7de370973a9870bb2abda
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2020-10-11 22:35:36 +0200

    Create puissance_recursif.py

---
+++ puissance_recursif.py
@@ -0,0 +1,14 @@
+def puissance(x:int,y:int)-> int:
+    """
+        fonction recursif pour calculer une puissance
+        prend un nombre entier et la puissance par laquelle on veux multiplier
+        retourne x puissance y
+        
+        >>>puissance(10,5)
+        100000
+    """
+    if y == 0:
+        return 1#cas de base
+    else:
+        return x*puissance(x,y-1)#cas de propagation
+print(puissance(10,5))
