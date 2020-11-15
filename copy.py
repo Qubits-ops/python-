205a20a41276c92d7f51ece16a280096fcff9c97
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2020-11-16 00:12:53 +0100

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,4 +1,4 @@
-def copy(a:list or str or int or dict)->list or str or int or dict:
+def copy(a:list or str or int or dict or tuple)->list or str or int or dict or tuple:
     if type(a) == list:
         i = [];
         for x in a:
@@ -16,6 +16,9 @@ def copy(a:list or str or int or dict)->list or str or int or dict:
     elif type(a) == str:
         strt = a
         return strt
+    elif type(a) == tuple:
+        tuple1 = a
+        return a
 li = [1,2,3,4,5]
 copi = copy(li)
 li.clear()
